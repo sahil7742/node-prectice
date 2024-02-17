af6dbd093605fb624902cbcbafd3895d6e77662e
sahil shrivastava <145384263+sahil7742@users.noreply.github.com>
2024-02-17 08:48:51 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,30 @@
+const name = "sahil shrivastava";
+console.log(name);
+// console.log(global);
+// sahil shrivastava
+// <ref *1> Object [global] {
+//   global: [Circular *1],
+//   clearImmediate: [Function: clearImmediate],
+//   setImmediate: [Function: setImmediate] {
+//     [Symbol(nodejs.util.promisify.custom)]: [Getter]
+//   },
+//   clearInterval: [Function: clearInterval],
+//   clearTimeout: [Function: clearTimeout],
+//   setInterval: [Function: setInterval],
+//   setTimeout: [Function: setTimeout] {
+//     [Symbol(nodejs.util.promisify.custom)]: [Getter]
+//   },
+//   queueMicrotask: [Function: queueMicrotask],
+//   structuredClone: [Getter/Setter],
+//   atob: [Getter/Setter],
+//   btoa: [Getter/Setter],
+//   performance: [Getter/Setter],
+//   fetch: [Function: fetch],
+//   crypto: [Getter]
+// }
+const add = 10+2;
+console.log (`The added value is ${add}`);
+const subs =10-2;
+console.log (`The substracted value is ${subs}`);
+
+
